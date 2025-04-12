3f300754f31c72c88c39f7d68377d3bb62f68b07
webkida-ss <a5113067.171452@gmail.com>
2025-04-12 10:10:34 +0900

    medium15.go修正

--- 11_leetcode/02_medium/00015/medium15.go
+++ 11_leetcode/02_medium/00015/medium15.go
@@ -7,49 +7,39 @@ import (
 
 func threeSum(nums []int) [][]int {
 
-	// ソート
 	sort.Ints(nums)
 
-	// 結果リスト
 	result := [][]int{}
 
-	// 3つの数を選ぶので、最後から2つ目までループ
 	for i := 0; i < len(nums)-2; i++ {
-		// 重複をスキップ (i > 0 は、i-1が存在することを確認するため)
-		if i > 0 && nums[i] == nums[i-1] {
+
+		left, right := i+1, len(nums)-1
+
+		if nums[i-1] == nums[i] && i > 0 {
 			continue
 		}
-		// 2つの数を選ぶので、最初と最後から1つずつループ
-		l, r := i+1, len(nums)-1
-		// rがlより大きい間ループ
-		for l < r {
-			// 合計が0なら結果に追加
-			sum := nums[i] + nums[l] + nums[r]
+
+		for left < right {
+			sum := nums[i] + nums[left] + nums[right]
 			if sum == 0 {
-				result = append(result, []int{nums[i], nums[l], nums[r]})
-				// 重複をスキップ (l < r は、l+1が存在することを確認するため)
-				for l < r && nums[l] == nums[l+1] {
-					// lをインクリメント（左ポインタを右に移動）
-					l++
+				result = append(result, []int{nums[i], nums[left], nums[right]})
+				// 重複削除
+				for left < right && nums[left] == nums[left+1] {
+					left++
 				}
-				// 重複をスキップ (l < r は、r-1が存在することを確認するため)
-				for l < r && nums[r] == nums[r-1] {
-					// rをデクリメント（右ポインタを左に移動）
-					r--
+				for left < right && nums[right-1] == nums[right] {
+					right--
 				}
-				// lをインクリメント（左ポインタを右に移動）
-				l++
-				// rをデクリメント（右ポインタを左に移動）
-				r--
+				left++
+				right--
 			} else if sum < 0 {
-				// lをインクリメント（左ポインタを右に移動）
-				l++
+				left++
 			} else {
-				// rをデクリメント（右ポインタを左に移動）
-				r--
+				right--
 			}
 		}
 	}
+
 	return result
 }
 
