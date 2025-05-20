e56149bb7dde66e1226cedd77450afceac617d3d
ShunSakakida <shun.sakakida@kenedix-st.com>
2025-05-21 00:38:45 +0900

    insertion-sort

--- 01_algorithm/sort/03_insertion/insertion-sort.go
+++ 01_algorithm/sort/03_insertion/insertion-sort.go
@@ -5,13 +5,13 @@ import "fmt"
 func insertionSort(arr []int) []int {
 	l := len(arr)
 	for i := 0; i < l; i++ {
-		tmp := arr[i]
+		tmp := arr[i] // 1個ずつずらすから値を保持しておく
 		j := i - 1
 		for j >= 0 && arr[j] > tmp {
-			arr[j+1] = arr[j]
+			arr[j+1] = arr[j] // 右にずらす
 			j--
 		}
-		arr[j+1] = tmp
+		arr[j+1] = tmp // j+1は帳尻合わせ
 	}
 	return arr
 }
