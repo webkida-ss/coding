7ab6ff838402d3037fb7bb687d2fd3ad76f30f9f
ShunSakakida <shun.sakakida@kenedix-st.com>
2025-06-22 16:39:12 +0900

    fix

--- 01_algorithm/sort/01_bubble/bubble_sort.dart
+++ 01_algorithm/sort/01_bubble/bubble_sort.dart
@@ -1,3 +1,17 @@
+void bubbleSort(List<int> nums){
+  for(var i=0; i< nums.length; i++){
+    for(var j=0; j< nums.length-i-1; j++){
+      if(nums[j] > nums[j+1]){
+        var tmp = nums[j];
+        nums[j] = nums[j+1];
+        nums[j+1] = tmp;
+      }
+    }
+  }
+}
+
 void main(){
-  print('Hello, world');
+  List<int> nums = [12, 11, 13, 5, 6, 7];
+  bubbleSort(nums);
+  print(nums);
 }
